8f400cfa8f290c572157f7674a0b5ee9e0cdaca9
jinwoo <realize2578@gmail.com>
2026-05-15 00:20:20 +0900

    feat : 커취

--- src/test/java/com/back/together02be/global/security/SecurityIntegrationTest.java
+++ src/test/java/com/back/together02be/global/security/SecurityIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.back.together02be.global.security;
 
-import com.back.together02be.global.base.BaseIntegrationTest;
 import com.back.together02be.global.util.JwtUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 })
 @AutoConfigureMockMvc
 @Import(SecurityIntegrationTest.TestProtectedController.class)
-class SecurityIntegrationTest extends BaseIntegrationTest {
+class SecurityIntegrationTest {
 
     private static final String SECRET = "test-secret-key-must-be-32-bytes!!";
 
